d355dd479845b65d17f3fb1b2296c94bef5123d9
turnipsundae <jeux168@gmail.com>
2016-04-18 20:50:50 -0700

    Fix regex bug in string

---
+++ app/src/test/java/com/laudev/android/splitpea/TotalsTest.java
@@ -0,0 +1,15 @@
+package com.laudev.android.splitpea;
+
+import org.junit.Test;
+
+/**
+ * Test MainActivity totals
+ */
+public class TotalsTest {
+    @Test
+    public void TotalsTest() throws Exception {
+
+    };
+
+
+}
