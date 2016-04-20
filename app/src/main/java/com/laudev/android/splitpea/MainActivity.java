f58da04e245d9e8ff0e2b47b153baa3ab2f94f02
turnipsundae <jeux168@gmail.com>
2016-04-19 18:47:23 -0700

    Add calculate total button for testing

--- app/src/main/java/com/laudev/android/splitpea/MainActivity.java
+++ app/src/main/java/com/laudev/android/splitpea/MainActivity.java
@@ -2,8 +2,13 @@ package com.laudev.android.splitpea;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mTaxEditText;
     private EditText mTipEditText;
     private TextView mTotalTextView;
+    private Button mCalcTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +32,41 @@ public class MainActivity extends AppCompatActivity {
         mSubtotalEditText = (EditText)findViewById(R.id.subtotal);
         mTaxEditText = (EditText)findViewById(R.id.tax);
         mTipEditText = (EditText)findViewById(R.id.tip);
-        mTotalTextView= (TextView)findViewById(R.id.total);
+        mTotalTextView = (TextView)findViewById(R.id.total);
+
+        // add button to calculate total for now
+        // remove this later
+        mCalcTotal = (Button)findViewById(R.id.calc_total);
+        mCalcTotal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateTotal();
+            }
+        });
+
+        // auto focus on subtotal
     }
 
     /*
-    * get subtotal, tax and tip entries from screen
-    * if values are not filled out, assumes default values
-    */
+        * get subtotal, tax and tip entries from screen
+        * if values are not filled out, assumes default values
+        */
     private void getParams() {
         //TODO get input parameters
+        subtotal = Float.parseFloat(mSubtotalEditText.getText().toString());
+        tax = Float.parseFloat(mTaxEditText.getText().toString());
+        tip = Float.parseFloat(mTipEditText.getText().toString());
     }
 
+    /*
+    * update total TextView based on input params
+    */
     private void updateTotal() {
         //TODO update total based on input parameters
         getParams();
+        total = subtotal * ( 1 + tax ) * ( 1 + tip );
+        DecimalFormat df = new DecimalFormat("#.00");
+        df.setRoundingMode(RoundingMode.CEILING);
+        mTotalTextView.setText(df.format(total));
     }
 }
