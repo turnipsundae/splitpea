a264c65ea3efab228a5545e75ff4b3c4b0a2d4cc
turnipsundae <jeux168@gmail.com>
2016-04-21 20:04:24 -0700

    Add ListView holder

--- app/src/main/java/com/laudev/android/splitpea/ListItemHolder.java
+++ app/src/main/java/com/laudev/android/splitpea/ListItemHolder.java
@@ -1,7 +1,17 @@
 package com.laudev.android.splitpea;
 
+import android.view.View;
+import android.widget.TextView;
+
 /**
  * Created by kevin on 4/21/16.
  */
 public class ListItemHolder {
+    private final TextView mName;
+    private final TextView mSubtotal;
+
+    public ListItemHolder(View view) {
+        mName = (TextView)view.findViewById(R.id.name_textview);
+        mSubtotal = (TextView)view.findViewById(R.id.subtotal_textview);
+    }
 }
