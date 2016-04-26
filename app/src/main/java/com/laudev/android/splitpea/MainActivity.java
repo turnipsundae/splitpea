8b7c3cb54a4d36d4b7ecf6ab1220260ddf14d4bb
turnipsundae <jeux168@gmail.com>
2016-04-25 21:30:23 -0700

    Update vars to mVars

--- app/src/main/java/com/laudev/android/splitpea/MainActivity.java
+++ app/src/main/java/com/laudev/android/splitpea/MainActivity.java
@@ -13,13 +13,15 @@ import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String PARAM_SUBTOTAL = "subtotal";
-    private final String PARAM_TOTAL = "total";
+    private final String PARAM_NEW_EVENT = "newEvent";
+    private final String PARAM_SUBTOTAL = "mSubtotal";
+    private final String PARAM_TOTAL = "mTotal";
 
-    private float subtotal;
+    private boolean newEvent = true;
+    private float mSubtotal;
     private float tax;
     private float tip;
-    private float total;
+    private float mTotal;
 
     private EditText mSubtotalEditText;
     private EditText mTaxEditText;
@@ -46,13 +48,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), AllocateActivity.class)
-                        .putExtra(PARAM_SUBTOTAL, subtotal)
-                        .putExtra(PARAM_TOTAL, total);
+                        .putExtra(PARAM_NEW_EVENT, newEvent)
+                        .putExtra(PARAM_SUBTOTAL, mSubtotal)
+                        .putExtra(PARAM_TOTAL, mTotal);
                 startActivity(intent);
             }
         });
 
-        // add button to calculate total for now
+        // add button to calculate mTotal for now
         // remove this later
         mCalcTotal = (Button)findViewById(R.id.calc_total);
         mCalcTotal.setOnClickListener(new View.OnClickListener() {
@@ -64,12 +67,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-        * get subtotal, tax and tip entries from screen
+        * get mSubtotal, tax and tip entries from screen
         * if values are not filled out, assumes default values
         */
     private void getParams() {
         if (mSubtotalEditText.getText() != null) {
-            subtotal = Float.parseFloat(mSubtotalEditText.getText().toString());
+            mSubtotal = Float.parseFloat(mSubtotalEditText.getText().toString());
         }
         if (mTaxEditText.getText() != null) {
             tax = Float.parseFloat(mTaxEditText.getText().toString());
@@ -80,14 +83,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-    * update total TextView based on input params
+    * update mTotal TextView based on input params
     */
     private void updateTotal() {
-        //TODO update total based on input parameters
+        //TODO update mTotal based on input parameters
         getParams();
-        total = subtotal * ( 1 + tax ) * ( 1 + tip );
+        mTotal = mSubtotal * ( 1 + tax ) * ( 1 + tip );
         DecimalFormat df = new DecimalFormat("#.00");
         df.setRoundingMode(RoundingMode.CEILING);
-        mTotalTextView.setText(df.format(total));
+        mTotalTextView.setText(df.format(mTotal));
     }
 }
